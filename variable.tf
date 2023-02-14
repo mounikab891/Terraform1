7499e8d3879d08ef2c625df50e8e0ac74dffdcd0
mounikab891 <113983011+mounikab891@users.noreply.github.com>
2023-02-14 12:10:30 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,27 +1,12 @@
 variable "region" {
-  default     = "ap-south-1"
+  default     = "*********"
   type        = string
   description = "The region you want to deploy the infrastructure in"
 }
 
-
-#variable "servicename" {
-#type = list(string)
-#default = test
-#}
-variable "filename" {
-  default = "datasets.csv"
-}
-#variable "servicename" {
-  #type = list(string)
-  #default = ["mb-meds-stage", "mb-medsff-stage"]
-#}
-
-
-
 variable "cluster_name" {
   type        = string
   description = "The name of the ECS cluster where service is to be autoscaled"
-  default     = "mb-stage-private"
+  default     = "******"
 }
 

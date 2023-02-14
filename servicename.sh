7a3b2913c487f269cf498c478d2b67c8a7ff39c8
mounikab891 <113983011+mounikab891@users.noreply.github.com>
2023-02-14 12:22:18 +0530

    Update servicename.sh

--- servicename.sh
+++ servicename.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
-cluster="mb-stage-private"
-profile="medibuddy"
-region="ap-south-1"
-#service="trino-coordinator"
+cluster="$1"
+profile="****"
+region="*****"
+#  getting list of services arn of cluster
 service_name=$(aws ecs list-services --cluster $cluster  --region $region --profile $profile --query "serviceArns")
-#service=$(aws ecs list-services --cluster $cluster  --region $region --profile $profile --query "serviceArns" | sed 's:.*/::' | tr -d "[]\","| sed '/^[[:space:]]*$/d')
 
-#for i in $service
-#do 
-#DC=$(aws ecs describe-services --services $service --profile $profile --cluster $cluster --region $region |  jq -r '.services[].desiredCount')
-#done
-#echo $DC
-#jq -n --arg DesiredCount "$DC" '{$DesiredCount}'
+#storing output in json format in the variable 
 echo $service_name | jq -c --arg service_name "$service_name" '{$service_name}'
-#echo $service_name | jq -c --arg service_name {"DesiredCount":$service_name}'

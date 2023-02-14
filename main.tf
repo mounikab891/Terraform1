1146f327391b3e72b8cf47ca140430c59561ed92
mounikab891 <113983011+mounikab891@users.noreply.github.com>
2023-02-14 12:07:11 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,94 +1,123 @@
 provider "aws" {
-  region                    = var.region
-  shared_credentials_files  = ["/Users/mounikabethu/.aws/credentials"]
-  profile                   = "medibuddy"
+  region                   = var.region
+  shared_credentials_files = ["/Users/mounikabethu/.aws/credentials"]
+  profile                  = "medibuddy"
 }
-
-resource "aws_appautoscaling_target" "stg-websiteapp" {
-  max_capacity       = 2
-  min_capacity       = 1
-  resource_id        = "service/stg-ecs-cluster/stg-websiteapp"
-  scalable_dimension = "ecs:service:DesiredCount"
+################################################################################
+########### creating autoscaling for services ##################################
+##################################################################################
+resource "aws_appautoscaling_target" "service-autoscale" {
+  for_each           = data.aws_ecs_service.service
   service_namespace  = "ecs"
+  resource_id        = split(":", each.value.arn)[5]
+  scalable_dimension = "ecs:service:DesiredCount"
   role_arn           = aws_iam_role.ecs-autoscale-role.arn
+  min_capacity       =  each.value.desired_count
+  max_capacity       =  each.value.desired_count*2
 }
-#######cpu######
-resource "aws_appautoscaling_policy" "stg-websiteapp_cpu" {
-  name               = "application-scaling-policy-cpu"
-  policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.stg-websiteapp.resource_id
-  scalable_dimension = aws_appautoscaling_target.stg-websiteapp.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.stg-websiteapp.service_namespace
+################################################################################
+########### fetching the servicename from shellscript ################################
+##################################################################################
 
-  target_tracking_scaling_policy_configuration {
+data "external" "example" {
+  program = ["bash","servicename.sh"]
+  }
+################################################################################
+######################## ecsservice datablock ######################################
+##################################################################################
+
+data "aws_ecs_service" "service" {
+  for_each           = toset(flatten([for k, v in data.external.example.result : jsondecode(v)]))
+  service_name       = each.value
+  cluster_arn        = "arn:aws:ecs:ap-south-1:247653494814:cluster/${var.cluster_name}"
+}
+################################################################################
+######################## cpu-scale-up-policy ######################################
+############################################################################
+resource "aws_appautoscaling_policy" "target-cpu-scaleup_policy" {
+  #for_each           = toset(flatten([for k, v in data.external.example.result : jsondecode(v)]))
+  for_each           = aws_appautoscaling_target.service-autoscale
+  name               = "${split("/", each.value.resource_id)[2]}-cpu_scaleup"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = each.value.resource_id 
+  scalable_dimension = each.value.scalable_dimension
+  service_namespace  = each.value.service_namespace
+    target_tracking_scaling_policy_configuration {
     predefined_metric_specification {
       predefined_metric_type = "ECSServiceAverageCPUUtilization"
     }
     target_value = 50
   }
-  depends_on = [aws_appautoscaling_target.stg-websiteapp]
-  
+  #depends_on = [aws_appautoscaling_target.service-autoscale]
 }
 
-
-
-###memory#####
-resource "aws_appautoscaling_policy" "stg-websiteapp_memory" {
-  name               = "application-scaling-policy-memory"
+################################################################################
+######################## memory-scale-up-policy ######################################
+############################################################################
+resource "aws_appautoscaling_policy" "target-memory-scaleup_policy" {
+  #for_each           = toset(flatten([for k, v in data.external.example.result : jsondecode(v)]))
+  for_each           = data.aws_ecs_service.service
+  name               = "${each.value.service_name}-memory_scaleup"
   policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.stg-websiteapp.resource_id
-  scalable_dimension = aws_appautoscaling_target.stg-websiteapp.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.stg-websiteapp.service_namespace
-
-  target_tracking_scaling_policy_configuration {
+  resource_id        = split(":", each.value.arn)[5]
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+    target_tracking_scaling_policy_configuration {
     predefined_metric_specification {
       predefined_metric_type = "ECSServiceAverageMemoryUtilization"
     }
-    target_value = 60
+    target_value = 50
   }
-  depends_on = [aws_appautoscaling_target.stg-websiteapp]
-   
+  #depends_on = [aws_appautoscaling_target.service-autoscale]
 }
-# CLOUDWATCH ALARM to monitor the cpu utilization of a service (creating alarams)
-  resource "aws_cloudwatch_metric_alarm" "stg-websiteapp-cpuUtilization" {
-  #count             = length(var.autoscale) > 0 ? 1 : 0
-  alarm_description = "Scale down alarm for ${var.name}"
-  namespace         = "AWS/ECS"
-  alarm_name        = "ecsautoscalingcputest"
-  #alarm_actions     = [aws_appautoscaling_policy.stg-websiteapp.arn]
 
+# CLOUDWATCH ALARM to monitor the cpu utilization of a service (creating alarams)
+  resource "aws_cloudwatch_metric_alarm" "target-cpu-scaleup-alaram" {
+  for_each           = data.aws_ecs_service.service
+    #count             = length(var.servicename) > 0 ? 1 : 0
+  #alarm_description = "Scale up alarm for ${each.value.service}"
+  namespace         = "AWS/ECS"
+  alarm_name        = "${each.value.service_name}-cpu-scaleup-alaram"
+  alarm_actions     = ["arn:aws:sns:ap-south-1:247653494814:AWS_SERVICES_METRICS"]
   comparison_operator = "GreaterThanOrEqualToThreshold"
-  threshold           = "1"
+  threshold           = "60"
   evaluation_periods  = "2"
   metric_name         = "CPUUtilization"
   period              = "120"
   statistic           = "Maximum"
   datapoints_to_alarm =  "2"
-
   dimensions = {
-    ClusterName = "stg-ecs-cluster"
-    ServiceName = "stg-websiteapp"
+    #count       = length(var.servicename)
+    ClusterName = "${var.cluster_name}"
+    ServiceName = "${each.value.service_name}"
   }
+  
 }
 
-# CLOUDWATCH ALARM to monitor the memory utilization of a service ( creating alarams)
-  resource "aws_cloudwatch_metric_alarm" "stg-websiteapp-MemoryUtilization" {
-  #count             = length(var.autoscale) > 0 ? 1 : 0
-  alarm_description = "Scale down alarm for ${var.name}"
+# CLOUDWATCH ALARM to monitor the memory utilization of a service (creating alarams)
+  resource "aws_cloudwatch_metric_alarm" "target-memory-scaleup-alaram" {
+  for_each           = data.aws_ecs_service.service
+    #count             = length(var.servicename) > 0 ? 1 : 0
+  #alarm_description = "Scale up alarm for ${each.value.service}"
   namespace         = "AWS/ECS"
-  alarm_name        = "ecsautoscaling_memoty_test"
-  #alarm_actions     = [aws_appautoscaling_policy.stg-websiteapp.arn]
-
+  alarm_name        = "${each.value.service_name}-memory-scaleup-alaram"
+  alarm_actions     = ["arn:aws:sns:ap-south-1:247653494814:AWS_SERVICES_METRICS"]
   comparison_operator = "GreaterThanOrEqualToThreshold"
-  threshold           = "1"
+  threshold           = "60"
   evaluation_periods  = "2"
   metric_name         = "MemoryUtilization"
   period              = "120"
   statistic           = "Maximum"
   datapoints_to_alarm =  "2"
-
   dimensions = {
-    ClusterName = "stg-ecs-cluster"
-    ServiceName = "stg-websiteapp"
+    #count       = length(var.servicename)
+    ClusterName = "${var.cluster_name}"
+    ServiceName = "${each.value.service_name}"
   }
+  
 }
+
+
+
+
+

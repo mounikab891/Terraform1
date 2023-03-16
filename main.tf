c33db73205386147be78e8e119bf861e6393b4d2
mounikab891 <113983011+mounikab891@users.noreply.github.com>
2023-03-16 10:56:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,11 +27,12 @@ data "aws_ecs_service" "service" {
   service_name       = each.value
   cluster_arn        = "arn:aws:ecs:ap-south-1:247653494814:cluster/${var.cluster_name}"
 }
+
 ################################################################################
 ######################## cpu-scale-up-policy ######################################
 ############################################################################
+  
 resource "aws_appautoscaling_policy" "target-cpu-scaleup_policy" {
-  #for_each           = toset(flatten([for k, v in data.external.example.result : jsondecode(v)]))
   for_each           = aws_appautoscaling_target.service-autoscale
   name               = "${split("/", each.value.resource_id)[2]}-cpu_scaleup"
   policy_type        = "TargetTrackingScaling"
@@ -39,87 +40,102 @@ resource "aws_appautoscaling_policy" "target-cpu-scaleup_policy" {
   scalable_dimension = each.value.scalable_dimension
   service_namespace  = each.value.service_namespace
     target_tracking_scaling_policy_configuration {
-    predefined_metric_specification {
-      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+      target_value = 80
+    customized_metric_specification {
+      #for_each           = aws_appautoscaling_target.service-autoscale
+      metric_name = "CPUUtilization"
+      namespace   = "AWS/ECS"
+      statistic   = "Maximum"
+      unit        = "Percent"
+      dimensions {
+        name  = "ClusterName"
+        value = "${var.mb-privatecluster-2}"
+      }
+      dimensions {
+        name  = "ServiceName"
+        value = "${split("/", each.value.resource_id)[2]}"
+      }
+      
     }
-    target_value = 50
+    
   }
-  #depends_on = [aws_appautoscaling_target.service-autoscale]
 }
+  
 
 ################################################################################
 ######################## memory-scale-up-policy ######################################
 ############################################################################
-  
 resource "aws_appautoscaling_policy" "target-memory-scaleup_policy" {
-  #for_each           = toset(flatten([for k, v in data.external.example.result : jsondecode(v)]))
-  for_each           = data.aws_ecs_service.service
-  name               = "${each.value.service_name}-memory_scaleup"
+  for_each           = aws_appautoscaling_target.service-autoscale
+  name               = "${split("/", each.value.resource_id)[2]}-memory_scaleup"
   policy_type        = "TargetTrackingScaling"
-  resource_id        = split(":", each.value.arn)[5]
-  scalable_dimension = "ecs:service:DesiredCount"
-  service_namespace  = "ecs"
+  resource_id        = each.value.resource_id 
+  scalable_dimension = each.value.scalable_dimension
+  service_namespace  = each.value.service_namespace
     target_tracking_scaling_policy_configuration {
-    predefined_metric_specification {
-      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
-    }
-    target_value = 50
+      target_value = 80
+    customized_metric_specification {
+      #for_each    = aws_appautoscaling_target.service-autoscale
+      metric_name = "MemoryUtilization"
+      namespace   = "AWS/ECS"
+      statistic   = "Maximum"
+      unit        = "Percent"
+      dimensions {
+        name  = "ClusterName"
+        value = "${var.mb-privatecluster-2}"
+      }
+
+        dimensions {
+        name  = "ServiceName"
+        value = "${split("/", each.value.resource_id)[2]}"
+      }
+      }
   }
-  #depends_on = [aws_appautoscaling_target.service-autoscale]
+
+  
 }
 
 ################################################################################
-###CLOUDWATCH ALARM to monitor the cpu utilization of a service (creating alarams)################
-############################################################################
- 
+##########CLOUDWATCH ALARM to monitor the cpu utilization of a service (creating alarams)################################
+############################################################################# 
   resource "aws_cloudwatch_metric_alarm" "target-cpu-scaleup-alaram" {
   for_each           = data.aws_ecs_service.service
-    #count             = length(var.servicename) > 0 ? 1 : 0
-  #alarm_description = "Scale up alarm for ${each.value.service}"
   namespace         = "AWS/ECS"
-  alarm_name        = "${each.value.service_name}-cpu-scaleup-alaram"
-  alarm_actions     = ["arn:aws:sns:ap-south-1:247653494814:AWS_SERVICES_METRICS"]
+  alarm_name        = "${each.value.service_name}-cpuUtilization"
+  alarm_actions     = ["arn:aws:sns:ap-south-1:247653494814:ecs-services-metrics"]
   comparison_operator = "GreaterThanOrEqualToThreshold"
-  threshold           = "60"
-  evaluation_periods  = "2"
+  threshold           = "80"
+  evaluation_periods  = "5"
   metric_name         = "CPUUtilization"
-  period              = "120"
+  period              = "360"
   statistic           = "Maximum"
-  datapoints_to_alarm =  "2"
+  datapoints_to_alarm =  "5"
   dimensions = {
-    #count       = length(var.servicename)
-    ClusterName = "${var.cluster_name}"
+    
+    ClusterName = "${var.mb-privatecluster-2}"
     ServiceName = "${each.value.service_name}"
   }
   
 }
 ################################################################################
-###CLOUDWATCH ALARM to monitor the memory utilization of a service (creating alarams)################
-############################################################################
-  
+##CLOUDWATCH ALARM to monitor the memory utilization of a service (creating alarams)##########
+############################################################################# 
   resource "aws_cloudwatch_metric_alarm" "target-memory-scaleup-alaram" {
   for_each           = data.aws_ecs_service.service
-    #count             = length(var.servicename) > 0 ? 1 : 0
-  #alarm_description = "Scale up alarm for ${each.value.service}"
   namespace         = "AWS/ECS"
-  alarm_name        = "${each.value.service_name}-memory-scaleup-alaram"
-  alarm_actions     = ["arn:aws:sns:ap-south-1:247653494814:AWS_SERVICES_METRICS"]
+  alarm_name        = "${each.value.service_name}-memoryUtilization"
+  alarm_actions     = ["arn:aws:sns:ap-south-1:247653494814:ecs-services-metrics"]
   comparison_operator = "GreaterThanOrEqualToThreshold"
-  threshold           = "60"
-  evaluation_periods  = "2"
+  threshold           = "80"
+  evaluation_periods  = "5"
   metric_name         = "MemoryUtilization"
-  period              = "120"
+  period              = "360"
   statistic           = "Maximum"
-  datapoints_to_alarm =  "2"
+  datapoints_to_alarm =  "5"
   dimensions = {
-    #count       = length(var.servicename)
-    ClusterName = "${var.cluster_name}"
+    
+    ClusterName = "${var.mb-privatecluster-2}"
     ServiceName = "${each.value.service_name}"
   }
   
 }
-
-
-
-
-
